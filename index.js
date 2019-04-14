87a388cbdbba3b50571de5c75d62a45ef39e2dcd
Alexander <alexanderjamesdev@gmail.com>
2019-04-14 17:19:35 +1200

    First commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+var clientSecret=process.env.CLIENT_SECRET;
+var clientId=process.env.CLIENT_ID;
+var port=process.env.PORT;
+var path = require('path'); 
+app.use(express.static('public'));
+var router = express.Router();
+const fetch = require("node-fetch");
+var theCode="";
+var refreshToken="";
+
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, '/views', 'index.html'));
+    theCode=req.query.code;
+});
+
+function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
+app.get('/callstrava', (req, res) => { 
+    let callUrl='https://www.strava.com/oauth/token?client_id='+clientId+'&client_secret='+clientSecret+'&code='+theCode+'&grant_type=authorization_code&scope=read_all&scope=activity:read_all';
+    fetch(callUrl,{ method: 'POST', body: 'a=1' })
+    .then(handleErrors)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        refreshToken=myJson.refresh_token;
+        return refreshToken;
+    })
+    .then(function(refreshToken){
+        var runs=[];
+        var url="https://www.strava.com/oauth/token?client_id="+clientId+"&client_secret="+clientSecret+"&grant_type=refresh_token&refresh_token="+refreshToken;
+        console.log("this is the url "+url);
+        return fetch(url,{ 
+            method: 'POST', body: 'a=1' 
+        })
+        .then(handleErrors)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            runs.push(myJson);
+            let currentAccessToken = runs[0].access_token;
+            url='https://www.strava.com/api/v3/athlete/activities/?access_token='+currentAccessToken+'&page=1&per_page=200';
+            runs=[];
+            return url;
+        })
+    })
+    .then (function(url){
+        fetch(url)
+        .then(handleErrors)
+        .then(function(response){
+        return response.json();
+        })
+        .then(function(myJson) {
+            res.send(myJson);
+        })
+    }).catch(function(error) {
+        let message={message:"Unable to reach Strava API"};
+        res.send(message);
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ public/js/callscript.js
@@ -0,0 +1,146 @@
+var runArray=[];
+var encodedRoutes=[];
+var savedNames=[];
+var savedRoutes=[];
+
+//ON PAGE LOAD
+function loadcall(){
+  //INITIALISE MAP
+  drawMap();
+
+  document.getElementById('change').innerHTML = "Accessing Strava API...";
+  
+  //CALL SERVER TO CALL API
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      runArray=xhr.response;
+      if(runArray.message!=undefined){
+        document.getElementById('change').innerHTML = runArray.message;
+      }
+      //LOAD PAGE ELEMENTS
+      else{
+        document.getElementById('change').innerHTML = "Strava data has been succesfully retrieved";
+      }
+      document.getElementById('dataprintbtn').innerHTML =
+      "<button id='saverun' type='button' class='btn btn-success'>Save this run</button><button id='clearsavedruns' type='button' class='btn btn-success'>Clear saved runs</button>";
+      //POPULATE DROP-DOWN MENU
+      allTheNames()
+      //EVENT LISTENERS
+      document.getElementById('saverun').addEventListener("mousedown",function(event){
+        let current = getCurrentID();
+        if(runArray[current].map.summary_polyline==null){
+          alert("No map for this run");
+        }else{
+          savedRoutes.push(runArray[current].map.summary_polyline);
+        }
+      });
+      document.getElementById('clearsavedruns').addEventListener("mousedown",function(event){
+        savedRoutes.length=0;
+        let current = getCurrentID();
+        drawMapOfCurrent(current);
+      });
+    }
+  }
+  xhr.open('GET', '/callstrava', true);
+  xhr.send(null);
+}
+
+//CREATE ACTIVITY STRING
+function activityString(menuNum){
+  var latestType=runArray[menuNum].type;
+  var latestName=runArray[menuNum].name;
+  var latestDistance=runArray[menuNum].distance;
+  var nameString="Selected activity is a "+latestType+", named "+latestName+", over a distance of "+latestDistance+"m.";
+  document.getElementById('results').innerHTML = nameString;
+}
+
+//MANAGE DROP DOWN MENU
+function allTheNames(){
+  if(runArray===[]){
+    document.getElementById('change').innerHTML = "data not loaded";
+  }else{
+    var nameArray=[];
+    for(let i=0;i<runArray.length;i++){
+      nameArray.push(runArray[i].name);
+    }
+    var select = document.getElementById("selectNumber"); 
+    for(var i = 0; i < nameArray.length; i++) {
+      var opt = nameArray[i];
+      var el = document.createElement("option");
+      el.textContent = opt;
+      el.value = opt;
+      select.appendChild(el);
+    }
+    select.addEventListener('change', function() {
+      var selected = document.getElementById("selectNumber").selectedIndex-1; 
+      if(selected>=0){
+        activityString(selected);
+        drawMapOfCurrent(selected);
+      }
+    }, false);
+  }
+}
+
+//TO GET CURRENT ID
+function getCurrentID(){
+  let current = document.getElementById("selectNumber").selectedIndex-1; 
+  return current;
+}
+
+//DRAW EMPTY MAP
+function drawMap(){
+  document.getElementById('contmap').innerHTML = "<div id='map' style='width: 100%; height: 100%;'></div>";
+  var map=null;
+  if(map===null){ 
+    map = L.map('map').setView([-36.85, 174.76], 13);
+  }else{
+  }
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 50,}).addTo(map);
+}
+
+//DRAW MAP WITH POLYLINES
+function drawMapOfCurrent(actNumber){
+  if(runArray[actNumber].map.summary_polyline==null){
+    alert("No map for this run");
+  }else{
+  document.getElementById('contmap').innerHTML = "<div id='map' style='width: 100%; height: 100%;'></div>";
+  var map=null;
+  if(map===null){ 
+    map = L.map('map').setView([-36.85, 174.76], 13);
+  }else{
+  }
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 50,}).addTo(map);
+  var results=null;
+  var polyline=null;  
+  encodedRoutes.push(runArray[actNumber].map.summary_polyline);
+  for (let saved of savedRoutes) {
+    let coordinates = L.Polyline.fromEncoded(saved).getLatLngs();
+    let theline=L.polyline(
+    coordinates,
+    {
+      color: '#EF4708',
+      weight: 3,
+      opacity: 0.8,
+      lineJoin: 'round'
+    }
+    ).addTo(map);
+    }
+  
+  for (let encoded of encodedRoutes) {
+    let coordinates = L.Polyline.fromEncoded(encoded).getLatLngs();
+    let theline=L.polyline(
+    coordinates,
+    {
+      color: '#088DEF',
+      weight: 3,
+      opacity: 0.8,
+      lineJoin: 'round'
+    }
+    ).addTo(map);
+    encodedRoutes.pop();
+    map.fitBounds(theline.getBounds());
+    }
+  } 
+  }      
